e1ebd813f118e25c98c5965a752bc9434fd6927c
Ajay-va <javvajiajayvarma222@gmail.com>
2021-08-06 14:31:39 +0530

    message3rd

--- SpringBootRealTimeProjectCurdOperations/src/main/java/com/example/demo/SpringBootRealTimeProjectCurdOperationsApplication.java
+++ SpringBootRealTimeProjectCurdOperations/src/main/java/com/example/demo/SpringBootRealTimeProjectCurdOperationsApplication.java
@@ -9,19 +9,6 @@ public class SpringBootRealTimeProjectCurdOperationsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRealTimeProjectCurdOperationsApplication.class, args);
 
-System.out.println("Hiii ajay");	
-
-System.out.println("Hiii vijay");
-
-System.out.println("Hiii navya");	
-
-System.out.println("Hiii kumar");
-
-System.out.println("Hiii ramana");	
-
-System.out.println("Hiii teja");
-
-System.our.println("Hiii hello");
 
 
 
