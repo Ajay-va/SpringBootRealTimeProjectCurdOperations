56a025b5fb3be76491f981649428b36dd4ef6b38
Ajay-va <javvajiajayvarma222@gmail.com>
2021-08-06 14:20:01 +0530

    added new one

--- SpringBootRealTimeProjectCurdOperations/src/main/java/com/example/demo/SpringBootRealTimeProjectCurdOperationsApplication.java
+++ SpringBootRealTimeProjectCurdOperations/src/main/java/com/example/demo/SpringBootRealTimeProjectCurdOperationsApplication.java
@@ -11,7 +11,15 @@ public class SpringBootRealTimeProjectCurdOperationsApplication {
 
 System.out.println("Hiii ajay");	
 
-System.out.println("Hiii ajay");
+System.out.println("Hiii vijay");
+
+System.out.println("Hiii navya");	
+
+System.out.println("Hiii kumar");
+
+System.out.println("Hiii ramana");	
+
+System.out.println("Hiii teja");
 
 }
 
